065a2ecdba6ef0d478e57a845960272de50fb969
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-02-25 00:20:44 +0000

    generate number

--- atv1/app/page.js
+++ atv1/app/page.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 function Text1(){
   return <h1>This is a Title</h1>  
 }
@@ -8,13 +12,45 @@ function Text2(){
 
 export default function Home() {
   const classroom = 1;
+  const [hide, setHide] = useState(false);
+  const [numA, setNumA] = useState(0);
+
+//  function handleClick(){
+//    hide ? setHide(false) : setHide(true);
+
+      // if(hide){
+      //   setHide(false);
+      // }else{
+      //   setHide(true)
+      // }
+//  }
+
+  function generateNumber(){
+    setNumA(Math.floor(Math.random() * (10)))
+   
+  }
 
   return (
     <>
-      <Text1 />
-      <Text2 />
+      {!hide && (
+        <>
+
+        <Text1 />
+        <Text2 />
+
+        </>
+      )}
 
       <p>This is {classroom} (first) class</p>
+
+      <button 
+        onClick = { () => hide ? setHide(false) : setHide(true) }>
+        {hide ? "Show" : "Hide"}
+      </button>
+
+      <h1>Number:</h1>
+      <p>{numA}</p>
+      <button onClick = { generateNumber } >Generate Number</button>
     </>
   );
 }
